d6b837af06e6cfad151a647c3094195b8b27c238
Pesci-Apu <155049962+Pesci-Apu@users.noreply.github.com>
2024-05-22 18:38:05 +0200

    Update Game Version: v.3.0.70.55

--- Offsets.hpp
+++ Offsets.hpp
@@ -1,17 +1,17 @@
-// Game Version: v.3.0.68.50
+// Game Version: v.3.0.70.55
 #pragma once
 constexpr long OFF_REGION = 0x140000000; //[Static]->Region
-constexpr long OFF_LEVEL = 0x17C6120; //[Miscellaneous]->LevelName
-constexpr long OFF_LOCAL_PLAYER = 0x2259868; //[Miscellaneous]->LocalPlayer
-constexpr long OFF_ENTITY_LIST = 0x1EAACC8; //[Miscellaneous]->cl_entitylist
-constexpr long OFF_GLOBAL_VARS = 0x17c5c20; //[Miscellaneous]->GlobalVars
-constexpr long OFF_NAMELIST = 0xc790230; //[Miscellaneous]->NameList
+constexpr long OFF_LEVEL = 0x17c7160; //[Miscellaneous]->LevelName
+constexpr long OFF_LOCAL_PLAYER = 0x225a8a8; //[Miscellaneous]->LocalPlayer
+constexpr long OFF_ENTITY_LIST = 0x1eabd08; //[Miscellaneous]->cl_entitylist
+constexpr long OFF_GLOBAL_VARS = 0x17c6c60; //[Miscellaneous]->GlobalVars
+constexpr long OFF_NAMELIST = 0xc7912b0; //[Miscellaneous]->NameList
 
 // Buttons
-constexpr long OFF_IN_ATTACK = 0x074dd240; //[Buttons]->in_attack
-constexpr long OFF_IN_JUMP = 0x074dd320; //[Buttons]->in_jump
-constexpr long OFF_IN_DUCK = 0x074dd420; //[Buttons]->in_duck
-constexpr long OFFSET_IN_FORWARD = 0x074dd170; //[Buttons]->in_forward
+constexpr long OFF_IN_ATTACK = 0x074de2a0; //[Buttons]->in_attack
+constexpr long OFF_IN_JUMP = 0x074de3a0; //[Buttons]->in_jump
+constexpr long OFF_IN_DUCK = 0x074de4a0; //[Buttons]->in_duck
+constexpr long OFFSET_IN_FORWARD = 0x074de1f0; //[Buttons]->in_forward
 constexpr long OFFSET_TRAVERSAL_START_TIME = 0x2af0; //[RecvTable.DT_LocalPlayerExclusive]->m_traversalStartTime
 constexpr long OFFSET_TRAVERSAL_PROGRESS = 0x2aec; //[RecvTable.DT_LocalPlayerExclusive]->m_traversalProgress
 constexpr long OFFSET_TRAVERSAL_RELEASE_TIME = 0x2af8; //[RecvTable.DT_LocalPlayerExclusive]->m_traversalReleaseTime
@@ -20,7 +20,7 @@ constexpr long OFFSET_WALL_RUN_CLEAR_TIME = 0x3598; //RecvTable.DT_LocalPlayerEx
 
 // Player
 constexpr long OFF_VIEW_MATRIX = 0x11a350; //[RecvTable.DT_Player]->ViewMatrix
-constexpr long OFF_VIEW_RENDER = 0x74dbfa8; //[RecvTable.DT_Player]->ViewRender
+constexpr long OFF_VIEW_RENDER = 0x74dd028; //[RecvTable.DT_Player]->ViewRender
 constexpr long OFF_VIEW_MODELS = 0x2d18; //[RecvTable.DT_Player]->m_hViewModels
 constexpr long OFF_ZOOMING = 0x1bd1; //[RecvTable.DT_Player]->m_bZooming
 constexpr long OFF_LOCAL_ORIGIN = 0x017c; //[DataMap.CBaseViewModel]->m_vecAbsOrigin
@@ -45,17 +45,16 @@ constexpr long OFF_YAW = 0x223c - 0x8; //[DataMap.C_Player]=>m_currentFramePlaye
 constexpr long OFF_NAMEINDEX = 0x38; //[RecvTable.DT_BaseEntity]
 constexpr long OFF_XPLEVEL = 0x36a4; //m_xp
 constexpr long OFF_SQUAD_ID = 0x0334; //m_squadID
-constexpr long OFF_GAMEMODE = 0x02291170; //mp_gamemode
+constexpr long OFF_GAMEMODE = 0x022921b0; //mp_gamemode
 
 // Weapon
 constexpr long OFF_WEAPON_HANDLE = 0x1934; //[RecvTable.DT_BaseCombatCharacter]->m_latestPrimaryWeapons
 constexpr long OFF_WEAPON_INDEX = 0x1778; //[RecvTable.DT_WeaponX]->m_weaponNameIndex
-constexpr long OFFSET_AMMO = 0x1580; //[DT_WeaponX_LocalWeaponData]->m_ammoInClip
+constexpr long OFFSET_AMMO = 0x1554; //[DataMap.CWeaponX]->m_ammoInClip
 constexpr long OFF_GRENADE_HANDLE = 0x1944; //[RecvTable.DT_Player]->m_latestNonOffhandWeapons
 constexpr long OFF_SKIN = 0x0d58; //[RecvTable.DT_BaseAnimating]->m_nSkin
 constexpr long OFF_WEAPON_DISCARDED = 0x1599; //[RecvTable.DT_WeaponX]->m_discarded
 constexpr long OFFSET_TIME_BASE = 0x2088; //[RecvTable.DT_Player]->m_currentFramePlayer.timeBase
-
 constexpr long OFF_PROJECTILESCALE = 0x1eac; //CWeaponX!m_flProjectileScale //[WeaponSettings]->projectile_gravity_scale + [WeaponSettingsMeta].base
 constexpr long OFF_PROJECTILESPEED = 0x1ea4; //CWeaponX!m_flProjectileSpeed //[WeaponSettings]->projectile_launch_speed + [WeaponSettingsMeta].base
 
@@ -64,4 +63,4 @@ constexpr long HIGHLIGHT_TYPE_SIZE = 0x34;
 constexpr long OFF_GLOW_THROUGH_WALL = 0x26c; //[DT_HighlightSettings].?
 constexpr long OFF_GLOW_FIX = 0x268;
 constexpr long OFF_GLOW_HIGHLIGHT_ID = 0x28C; //[DT_HighlightSettings].m_highlightServerActiveStates
-constexpr long OFF_GLOW_HIGHLIGHTS = 0xade4bc0; //
+constexpr long OFF_GLOW_HIGHLIGHTS = 0xade5c40; //
