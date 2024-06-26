7d856cb3d3b7c272bdf70bd5eff5bc557c3ecfc0
Pesci-Apu <155049962+Pesci-Apu@users.noreply.github.com>
2024-06-26 17:27:36 +0200

    Update Player.hpp

--- Player.hpp
+++ Player.hpp
@@ -100,7 +100,7 @@ struct Player {
             spctrBase =  mem::Read<uint64_t>(OFF_REGION + OFF_ENTITY_LIST + ((spctrIndex & 0xFFFF) << 5), "Spectator Base");
             plyrDataTable = mem::Read<int>(base + OFF_NAMEINDEX, "Player Data Table");
             spectators = mem::Read<uint64_t>(OFF_REGION + OFF_SPECTATOR_LIST, "spectators");
-            spctrIndex = mem::Read<int>(spectators + plyrDataTable * 8 + 0x964, "Spectator Index");
+            spctrIndex = mem::Read<int>(spectators + plyrDataTable * 8 + 0x974, "Spectator Index");
             name = mem::ReadString(base + OFF_NAME, 1024, "Player name");
             teamNumber = mem::Read<int>(base + OFF_TEAM_NUMBER, "Player teamNumber");
             currentHealth = mem::Read<int>(base + OFF_CURRENT_HEALTH, "Player currentHealth");
