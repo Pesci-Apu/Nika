f1e23e7e64df77d746abc985bfe474e4476f3392
Pesci-Apu <155049962+Pesci-Apu@users.noreply.github.com>
2024-08-07 00:11:55 +0200

    Update Game Version: v.3.0.76.22

--- Weapons.hpp
+++ Weapons.hpp
@@ -4,38 +4,38 @@ constexpr int WEAPON_HANDS = 120;
 constexpr int WEAPON_SENTINEL = 1;
 constexpr int WEAPON_CHARGE_RIFLE = 87;
 constexpr int WEAPON_LONGBOW = 90;
-constexpr int WEAPON_WINGMAN = 114;
+constexpr int WEAPON_WINGMAN = 115;
 //Shotgun ammo weapons
 constexpr int WEAPON_MOZAMBIQUE = 102;
-constexpr int WEAPON_EVA8 = 93;
+constexpr int WEAPON_EVA8 = 92;
 constexpr int WEAPON_PEACEKEEPER = 109;
 constexpr int WEAPON_MASTIFF = 101;
 //Light ammo weapons
-constexpr int WEAPON_P2020 = 111;
+constexpr int WEAPON_P2020 = 112;
 constexpr int WEAPON_RE45 = 85;
 constexpr int WEAPON_ALTERNATOR = 84;
-constexpr int WEAPON_R99 = 110;
 constexpr int WEAPON_R301 = 0;
-constexpr int WEAPON_SPITFIRE = 112;
+constexpr int WEAPON_SPITFIRE = 113;
 constexpr int WEAPON_G7 = 95;
 //Heavy ammo weapons
-constexpr int WEAPON_CAR = 118;
+constexpr int WEAPON_CAR = 119;
 constexpr int WEAPON_RAMPAGE = 6;
-constexpr int WEAPON_3030 = 117;
+constexpr int WEAPON_3030 = 118;
 constexpr int WEAPON_HEMLOCK = 96;
 constexpr int WEAPON_FLATLINE = 94;
 constexpr int WEAPON_PROWLER = 107;
 //Energy ammo weapons
-constexpr int WEAPON_NEMESIS = 119;
-constexpr int WEAPON_VOLT = 116;
-constexpr int WEAPON_TRIPLE_TAKE = 113;
+constexpr int WEAPON_NEMESIS = 120;
+constexpr int WEAPON_VOLT = 117;
+constexpr int WEAPON_TRIPLE_TAKE = 114;
 constexpr int WEAPON_LSTAR = 99;
-constexpr int WEAPON_DEVOTION = 89;
 constexpr int WEAPON_HAVOC = 91;
 //Legendary ammo weapons
+constexpr int WEAPON_R99 = 111;
+constexpr int WEAPON_DEVOTION = 89;
 constexpr int WEAPON_BOCEK = 2;
 constexpr int WEAPON_KRABER = 98;
-constexpr int WEAPON_THROWING_KNIFE = 173;
+constexpr int WEAPON_THROWING_KNIFE = 176;
 constexpr int WEAPON_THERMITE_GRENADE = 164;
 constexpr int WEAPON_RAMPART_SHEILA_STATIONARY = 14;
 
